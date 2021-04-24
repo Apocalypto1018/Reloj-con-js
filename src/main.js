fabcdced35c840eaf924122f97eabf3bad9abc34
Miguel Padrino <mikehathsin@gmail.com>
2021-04-24 17:34:57 -0400

    format the files

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
 const startButton = document.getElementById("start_Clock");
 const stopButton = document.getElementById("stop_Clock");
-const oneSecond= 1000;
-let intervalId=null;
+const oneSecond = 1000;
+let intervalId = null;
 
-let runClock = ()=>{
+let runClock = () => {
+  intervalId = setInterval(() => {
+    let systemTime = new Date();
+    let hour = systemTime.getHours();
+    let minute = systemTime.getMinutes();
+    let second = systemTime.getSeconds();
 
-    intervalId = setInterval(()=>{
+    let showTime = `${hour} : ${minute}  : ${second}`;
 
-        let systemTime = new Date();
-        let hour = systemTime.getHours();
-        let minute = systemTime.getMinutes();
-        let second = systemTime.getSeconds();    
+    document.formMain.showClock.value = showTime;
 
-        let showTime =`${hour} : ${minute}  : ${second}`; 
-        
-        document.formMain.showClock.value = showTime;
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }, oneSecond);
+};
 
-        startButton.disabled=true;
-        stopButton.disabled=false;
+let stopClock = () => {
+  clearInterval(intervalId);
 
-    },oneSecond); 
-}
+  startButton.disabled = false;
+  stopButton.disabled = true;
 
-let stopClock = ()=>{
-    clearInterval(intervalId);
-    
-    startButton.disabled=false;
-    stopButton.disabled=true;
-
-    document.formMain.showClock.value = 'clock stopped';
-}
+  document.formMain.showClock.value = "clock stopped";
+};
 
 startButton.addEventListener("click", runClock);
 stopButton.addEventListener("click", stopClock);
